9e58b3f054a071cb769b8f8f7cf0207cbb36d9d4
Wesley Tiozzo <tiozzo.wesley1@gmail.com>
2022-02-25 17:35:42 -0300

    14 - Updated ClientCrud.jsx

--- frontend/src/components/client/ClientCrud.jsx
+++ frontend/src/components/client/ClientCrud.jsx
@@ -39,11 +39,85 @@ export default class ClientCrud extends Component {
     list.unshift(client);
     return list;
   }
+
+  updateField(event) {
+    const client = { ...this.state.client }
+    client[event.target.name] = event.target.value;
+    this.setState({ client });
+  }
+
+  renderForm() {
+    return(
+      <div className="form">
+        <div className="row">          
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>Nome</label>
+              <input type="text" className="form-control"
+                name="name"
+                value={this.state.client.name}
+                onChange={e => this.updateField(e)}
+                placeholder="Digite o nome:" />
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>Telefone</label>
+              <input type="text" className="form-control"
+                name="phone"
+                value={this.state.client.phone}
+                onChange={e => this.updateField(e)}
+                placeholder="Digite o telefone:" />
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>E-mail</label>
+              <input type="text" className="form-control"
+                name="email"
+                value={this.state.client.email}
+                onChange={e => this.updateField(e)}
+                placeholder="Digite o email:" />
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>CNPJ</label>
+              <input type="text" className="form-control"
+                name="cnpj"
+                value={this.state.client.cnpj}
+                onChange={e => this.updateField(e)}
+                placeholder="Digite o CNPJ:" />
+            </div>
+          </div>
+        </div>
+
+        <hr />
+        <div className="row">
+          <div className="col-12 d-flex justify-content-end">
+            <button className="btn btn-primary"
+              onClick={e => this.save(e)}>
+              Salvar
+            </button>
+
+            <button className="btn btn-secondary ml-2"
+              onClick={e => this.clear(e)}>
+              Cancelar
+            </button>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
   
   render() {
     return (
       <Main {...headerProps}>
-        Cadastro de Clientes
+        {this.renderForm()}
       </Main>
     )
   }
