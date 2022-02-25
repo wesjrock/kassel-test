38f735d45f1202a7759759efd242f481a8824983
Wesley Tiozzo <tiozzo.wesley1@gmail.com>
2022-02-25 17:02:48 -0300

    13 - Updated ClientCrud.jsx and Added axios

--- frontend/src/components/client/ClientCrud.jsx
+++ frontend/src/components/client/ClientCrud.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Main from "../template/Main";
 
 const headerProps = {
@@ -7,7 +8,38 @@ const headerProps = {
   subtitle: "Cadastro de clientes: Incluir, Listar, Alterar e Excluir"
 }
 
+const baseUrl = "http://localhost:3001/clients";
+
+const initialState = {
+  client: { name: "", phone: "", email: "", cnpj: "" },
+  list: []
+}
+
 export default class ClientCrud extends Component {
+  
+  state = { ...initialState }
+
+  clear() {
+    this.setState( {client: initialState.client} )
+  }
+
+  save() {
+    const client = this.state.client;
+    const method = client.id ? 'put' : 'post';
+    const url = client.id ? `${baseUrl}/${client.id}` : baseUrl;
+    axios[method](url, client)
+      .then(resp => {
+        const list = this.getUpdatedList(resp.data);
+        this.setState({ client: initialState.client, list})
+      })
+  }
+
+  getUpdatedList(client) {
+    const list = this.state.list.filter(c => c.id !== client.id);
+    list.unshift(client);
+    return list;
+  }
+  
   render() {
     return (
       <Main {...headerProps}>
