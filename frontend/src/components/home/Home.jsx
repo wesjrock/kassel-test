f2b46209636f026f47bb32fe7e7eb63b954b573b
Wesley Tiozzo <tiozzo.wesley1@gmail.com>
2022-02-25 15:53:20 -0300

    10 - Modified Main.jsx and added Home.jsx component

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props =>
+  <Main icon="home" title="Início" subtitle="Kassel Labs Frontend Test">
+    <div className="display-4">Bem Vindo!</div>
+    <hr />
+    <p className="mb-0">Sistema para cadastrar clientes</p>
+  </Main>
